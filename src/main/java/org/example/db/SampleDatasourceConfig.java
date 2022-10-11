29b1b65aec7d3eeb4f31fe7a933d45d0072bfdf1
j1zg <lwpro2@live.com>
2022-10-11 15:34:39 +0800

    add the post construct

--- src/main/java/org/example/db/SampleDatasourceConfig.java
+++ src/main/java/org/example/db/SampleDatasourceConfig.java
@@ -1,19 +1,27 @@
 package org.example.db;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 /**
  * @author lwpro
  */
 @Configuration
+@Slf4j
 public class SampleDatasourceConfig {
-    @Bean
+
+    @PostConstruct
+    public void init() {
+        log.info("some set up to prepare the datasource and other beans here...");
+    }
+    @Bean({"com.example.RODataSource"})
     DataSource sampleDataSource() {
         return DataSourceBuilder.create()
                 .username("admin")
